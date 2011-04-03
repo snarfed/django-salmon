6d7ec7cce69053324e5b4b48de8ccb0ee3951f6d
Paul Osman <paul@eval.ca>
2011-04-03 14:11:27 -0700

    Respond with a 400, not a 401 when magic signature verification fails

--- salmon/views.py
+++ salmon/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
@@ -14,7 +14,7 @@ def endpoint(request):
     # verify that data was signed by sender
     author_uri = utils.parse_author_uri_from_atom(parsed['data'])
     if not magicsigs.verify(author_uri, parsed['data'], parsed['sig']):
-        return HttpResponseForbidden()
+        return HttpResponseBadRequest("Could not verify magic signature.")
 
     # hand waving on mime_type right now, but seems like this'd be
     # a decent interface.
